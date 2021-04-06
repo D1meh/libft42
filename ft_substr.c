f0d509b1fd70ae041104af01e63ca014859585f6
D1meh <80787893+D1meh@users.noreply.github.com>
2021-04-06 13:54:53 +0200

    Add files via upload
    malloc KO corrected

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mtaouil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 11:46:14 by mtaouil           #+#    #+#             */
-/*   Updated: 2021/03/26 08:22:25 by mtaouil          ###   ########.fr       */
+/*   Updated: 2021/03/26 10:07:04 by mtaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s || start >= (unsigned int)ft_strlen(s))
 		return (ft_strdup(""));
+	if (ft_strlen(&(s[start])) < len)
+		len = ft_strlen(&(s[start]));
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
 		return (NULL);
